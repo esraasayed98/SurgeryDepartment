6028f0b9800c28197c39902b1c9b05a7c12ecda4
esraasayed98 <sayedesraa645@gmail.com>
2019-12-23 16:51:03 +0200

    U

---
+++ SurgeryDepartment/test.py
@@ -0,0 +1,152 @@
+import mysql.connector
+from flask import Flask, redirect, url_for, request,render_template,flash
+from flask_table import Table, Col
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="mysql",
+  database="SurgeryDepartment"
+)
+mycursor = mydb.cursor()
+app = Flask(__name__)
+app.secret_key= 'dont tell anyone'
+@app.route('/')
+def home():
+	return render_template('home.html')
+@app.route('/signin', methods=['POST','GET'])
+def signin():
+	if request.method == 'POST': 
+		username = request.form['user_name']
+		password = request.form['pass_word']
+		if str(request.form['user_name'])=='Ahmed':
+			return render_template('index.html')
+		else:	   
+			mycursor.execute("SELECT * FROM Doctors WHERE user_name='" +username+ "' and pass_word='" +password+ "' ")
+			data=mycursor.fetchone()
+			if data is None:
+				return render_template('sign-in.html',msg='Username or Password is wrong!') 
+			else:
+				return render_template('doctors.html')
+	else:
+		return render_template('sign-in.html') 
+@app.route('/addsurgeon' ,methods = ['POST', 'GET'])
+def addsurgeon():
+	if request.method == 'POST': 
+		username = request.form['user_name']
+		passwd = request.form['pass_word']
+		name = request.form['name']
+		id= request.form['D_id']
+		age=request.form['age']
+		gender=request.form['gender']
+		stdate=request.form['start_date']
+		degree=request.form['degree']
+		department=request.form['department']
+		phone=request.form['phone']
+		mail=request.form['mail']
+		sql = "INSERT INTO Doctors(user_name,pass_word,name ,D_id,age,gender,start_date,degree,department,phone,mail) VALUES (%s,%s,%s, %s ,%s ,%s,%s, %s ,%s,%s, %s )"
+		val = (username,passwd,name,id,age,gender,stdate,degree,department,phone,mail)
+		mycursor.execute(sql, val)
+		mydb.commit()   
+		return render_template('index.html')
+	else:
+		return render_template('add.html')
+@app.route('/deletedoctor' , methods=['POST' ,'GET'])
+def deletedoctor():
+	if request.method=='POST':
+		id=request.form['D_id']
+		mycursor.execute("SELECT * FROM Doctors WHERE D_id='" +id+ "'")
+		sid=mycursor.fetchone()
+		if sid is None:
+			flash("couldn't find the doctor" )
+			return render_template('deletedoctor.html')
+		else:
+			flash("Data Deleted successfully")
+			mycursor.execute("DELETE FROM Doctors WHERE D_id = '" +id+"' ")
+			mydb.commit()
+			return render_template('index.html')
+	else:
+		return render_template('deletedoctor.html')
+@app.route('/head')
+def head():
+	return render_template('index.html')
+
+@app.route('/viewsurgeons',methods = ['POST', 'GET'])
+def viewsurgeons():
+	if request.method == 'POST':
+		return render_template('index.html')
+	else:
+		mycursor.execute("SELECT * FROM Doctors")
+		row_headers=[x[0] for x in mycursor.description] 
+		myresult = mycursor.fetchall()
+		data={
+		'message':"data retrieved",
+		'rec':myresult,
+		'header':row_headers
+		}
+		return render_template('view.html',data=data)	     	      	
+@app.route('/applyforsurgery' ,methods = ['POST', 'GET'])
+def applyforsurgery():
+	if request.method == 'POST': 
+		name = request.form['name']
+		pid = request.form['P_id']
+		Did=request.form['d_id']
+		age=request.form['age']
+		gender=request.form['gender']
+		VD=request.form['visit_date']
+		SD=request.form['surgery_date']
+		ST=request.form['surgery_time']
+		urgency=request.form['urgency']
+		sql = "INSERT INTO Patients(name, P_id ,d_id ,age,gender,visit_date,surgery_date,surgery_time,urgency) VALUES (%s,%s,%s, %s ,%s ,%s,%s, %s ,%s )"
+		val = (name,pid,Did,age,gender,VD,SD,ST,urgency)
+		mycursor.execute(sql, val)
+		mydb.commit() 
+		return render_template('apply_for_surgery.html') 
+	else:
+			return render_template('apply_for_surgery.html')
+
+# @app.route('/searcdoctor')
+# def searcdoctor():
+#    return render_template('patient_id.html')
+#####to print the patients info according to doctor id and dep id
+@app.route('/patientinfo',methods = ['POST', 'GET'])
+def patientinfo():
+	if request.method == 'POST': 
+		id=request.form['d_id']
+		# print(id)
+		mycursor.execute( "SELECT * FROM Patients WHERE D_id= '" +id+ "' ")
+		row_headers=[x[0] for x in mycursor.description] 
+		myresult = mycursor.fetchall()
+		data={
+		'message':"data retrieved",
+		'rec':myresult,
+		'header':row_headers
+		}
+		return render_template('patients_data.html',data=data)
+	else:
+		return render_template('patient_id.html')
+
+
+# @app.route('/searchroom')
+# def searchroom():
+#    return render_template('findroom.html')
+#####to print the room info according to room id and dep id
+@app.route('/findrooms',methods = ['POST', 'GET'])
+def findrooms():
+   if request.method == 'POST': 
+      Room_ID = request.form['Room ID']
+      Department_ID = request.form['Department ID']
+      print(Room_ID, Department_ID )
+      mycursor.execute( "SELECT * FROM OR_Rooms WHERE Room_id= '" +Room_ID+ "' AND Dep_id = '" +Department_ID+ "' ")
+      row_headers=[x[0] for x in mycursor.description] 
+      myresult = mycursor.fetchall()
+      data={
+         'message':"data retrieved",
+         'rec':myresult,
+         'header':row_headers
+      }
+      return render_template('viewroom.html',data=data)
+   else:
+      return render_template('findroom.html')
+
+if __name__ == '__main__':
+	app.run(debug=True , port=9000)
